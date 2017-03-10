2b17475ffba0136b1ef6de911abd4551da26318d
Dmitry Fedosov <dimafeng@gmail.com>
2017-03-09 20:40:49 -0500

    Grand Canyon

---
+++ themes/dimafeng/source/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.article-full-content p, .article-full-content h2, .article-full-content h3, blockquote, .article-full-content ul').each(function() {
+      $(this).addClass('container');
+    });
+});
